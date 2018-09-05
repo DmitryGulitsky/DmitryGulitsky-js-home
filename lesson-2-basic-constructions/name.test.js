7afbaeda0422f8a6c06c0671e32153d4e1b03939
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-05 20:27:50 +0300

    Add arithmetic, bonding, comparsion operations

--- lesson-2-basic-constructions/name.test.js
+++ lesson-2-basic-constructions/name.test.js
@@ -1,42 +1,216 @@
 describe('Typeof methods', () => {
   it('Typeof string Positive', () => {
-    const str = 'abc';
-    expect(typeof(str)).toBe('string');
+    const somethingUnknown = 'abc';
+    expect(typeof(somethingUnknown)).toBe('string');
   });
   it('Typeof string Negative', () => {
-    const str = 'abc';
-    expect(typeof(str)).toBe('number');
+    const somethingUnknown = 'abc';
+    expect(typeof(somethingUnknown)).toBe('number');
   });
   it('Typeof number Positive', () => {
-    const str = 10;
-    expect(typeof(str)).toBe('number');
+    const somethingUnknown = 10;
+    expect(typeof(somethingUnknown)).toBe('number');
   });
   it('Typeof number Negative', () => {
-    const str = 10;
-    expect(typeof(str)).toBe('boolean');
+    const somethingUnknown = 10;
+    expect(typeof(somethingUnknown)).toBe('boolean');
   });
   it('Typeof boolean Positive', () => {
-    const str = true;
-    expect(typeof(str)).toBe('boolean');
+    const somethingUnknown = true;
+    expect(typeof(somethingUnknown)).toBe('boolean');
   });
   it('Typeof boolean Negative', () => {
-    const str = 'abc';
-    expect(typeof(str)).toBe('number');
+    const somethingUnknown = 'abc';
+    expect(typeof(somethingUnknown)).toBe('number');
   });
   it('Typeof undefined Positive', () => {
-    const str = undefined;
-    expect(typeof(str)).toBe('undefined');
+    const somethingUnknown = undefined;
+    expect(typeof(somethingUnknown)).toBe('undefined');
   });
   it('Typeof undefined Negative', () => {
-    const str = 'abc';
-    expect(typeof(str)).toBe('number');
+    const somethingUnknown = 'abc';
+    expect(typeof(somethingUnknown)).toBe('number');
   });
   it('Typeof object Positive', () => {
-    const str = {};
-    expect(typeof(str)).toBe('object');
+    const somethingUnknown = {};
+    expect(typeof(somethingUnknown)).toBe('object');
   });
   it('Typeof object Negative', () => {
-    const str = 'abc';
-    expect(typeof(str)).toBe('object');
+    const somethingUnknown = 'abc';
+    expect(typeof(somethingUnknown)).toBe('object');
   });
 });
+
+describe('Arithmetic operations', () => {
+  it('Pre minus one Positive', () => {
+    let somethingUnknown = 10;
+    expect(--somethingUnknown).toBe(9);
+  });
+  it('Pre minus one Negative', () => {
+    let somethingUnknown = 10;
+    expect(--somethingUnknown).toBe(10);
+  });
+
+  it('Pre plus one Positive', () => {
+    let somethingUnknown = 10;
+    expect(++somethingUnknown).toBe(11);
+  });
+  it('Pre plus one Negative', () => {
+    let somethingUnknown = 10;
+    expect(++somethingUnknown).toBe('Some words');
+  });
+
+  it('Minus one Positive', () => {
+    let somethingUnknown = 10;
+    expect(somethingUnknown--).toBe(9);
+  });
+  it('Minus one Negative', () => {
+    let somethingUnknown = 10;
+    expect(somethingUnknown--).toBe(10);
+  });
+
+  it('Plus one Positive', () => {
+    let somethingUnknown = 10;
+    expect(somethingUnknown++).toBe(11);
+  });
+  it('Plus one Negative', () => {
+    let somethingUnknown = 10;
+    expect(somethingUnknown++).toBe(10);
+  });
+
+  it('Minus Positive', () => {
+    let somethingUnknown = 20;
+    somethingUnknown -= 10;
+    expect(somethingUnknown).toBe(10);
+  });
+  it('Minus Negative', () => {
+    let somethingUnknown = 20;
+    somethingUnknown -= 10;
+    expect(somethingUnknown).toBe(100);
+  });
+
+  it('Plus Positive', () => {
+    let somethingUnknown = 10;
+    somethingUnknown += 10;
+    expect(somethingUnknown).toBe(20);
+  });
+  it('Plus Negative', () => {
+    let somethingUnknown = 10;
+    somethingUnknown += 10;
+    expect(somethingUnknown).toBe(100);
+  });
+
+  it('Multiplication Positive', () => {
+    let somethingUnknown = 10;
+    somethingUnknown *= 10;
+    expect(somethingUnknown).toBe(100);
+  });
+  it('Multiplication Negative', () => {
+    let somethingUnknown = 10;
+    somethingUnknown *= 500;
+    expect(somethingUnknown).toBe(100);
+  });
+
+  it('Division Positive', () => {
+    let somethingUnknown = 10;
+    somethingUnknown /= 10;
+    expect(somethingUnknown).toBe(1);
+  });
+  it('Division Negative', () => {
+    let somethingUnknown = 10;
+    somethingUnknown /= 10;
+    expect(somethingUnknown).toBe(100);
+  });
+})
+
+describe('Bonding operations', () => {
+  it('Pre minus one Positive', () => {
+    const firstWord = 'Milky';
+    const secondWord = 'way';
+    expect(firstWord + secondWord).toBe('Milkyway');
+  });
+  it('Pre minus one Positive', () => {
+    const firstWord = 'Milky';
+    const secondWord = 'way';
+    expect(firstWord + secondWord).toBe('Nuts');
+  });
+  it('Pre minus one Positive', () => {
+    let firstWord = 'Milky';
+    const secondWord = 'way';
+    firstWord += secondWord;
+    expect(firstWord).toBe('Milkyway');
+  });
+  it('Pre minus one Positive', () => {
+    let firstWord = 'Milky';
+    const secondWord = 'way';
+    firstWord += secondWord;
+    expect(firstWord).toBe('Milky way');
+  });
+});
+
+describe('Comparsion operations', () => {
+  it('Operation more Positive', () => {
+    const firstNumber = 11;
+    const secondNumber = 10;
+    const operationResult = (firstNumber > secondNumber);
+    expect(operationResult).toBe(true);
+  });
+  it('Operation more Negative', () => {
+    const firstNumber = 11;
+    const secondNumber = 10;
+    const operationResult = (firstNumber < secondNumber);
+    expect(operationResult).toBe(true);
+  });
+
+  it('Operation less Positive', () => {
+    const firstNumber = 10;
+    const secondNumber = 11;
+    const operationResult = (firstNumber < secondNumber);
+    expect(operationResult).toBe(true);
+  });
+  it('Operation less Negative', () => {
+    const firstNumber = 11;
+    const secondNumber = 10;
+    const operationResult = (firstNumber < secondNumber);
+    expect(operationResult).toBe(true);
+  });
+
+  it('Operation more or equally Positive', () => {
+    const firstNumber = 11;
+    const secondNumber = 11;
+    const operationResult = (firstNumber >= secondNumber);
+    expect(operationResult).toBe(true);
+  });
+  it('Operation more or equally Negative', () => {
+    const firstNumber = 11;
+    const secondNumber = 11;
+    const operationResult = (firstNumber > secondNumber);
+    expect(operationResult).toBe(true);
+  });
+
+  it('Operation less or equally Positive', () => {
+    const firstNumber = 10;
+    const secondNumber = 11;
+    const operationResult = (firstNumber <= secondNumber);
+    expect(operationResult).toBe(true);
+  });
+  it('Operation less or equally Negative', () => {
+    const firstNumber = 11;
+    const secondNumber = 10;
+    const operationResult = (firstNumber <= secondNumber);
+    expect(operationResult).toBe(true);
+  });
+
+
+
+})
+describe('String methods', () => {
+  it('StringUpperCasePositive', () => {
+    const somethingUnknown = 'abc';
+    expect(somethingUnknown.toLocaleUpperCase()).toBe('ABC');
+  });
+  it('StringUpperCaseNegative', () => {
+    const somethingUnknown = 'abc';
+    expect(somethingUnknown.toLocaleUpperCase()).toBe('abc');
+  });
+})
