40c55ccd1c5400bbb88da28103a0d4ff33acacc0
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-12 21:00:34 +0300

    Complete task2

--- lesson-4-code-execution/task2.js
+++ lesson-4-code-execution/task2.js
@@ -1,9 +1,10 @@
 function multiplyNumeric(obj) {
   for (var key in obj) {
-    if (typeof(obj[key]) === 'number') {
+    if (typeof obj[key] === 'number') {
       obj[key] *= 2;
     }
   }
+  return obj;
 }
-var image = {width: 100,height: 400,title: 'Cool image'};
+var image = {width: 100, height: 400, title: 'Cool image'};
 console.log(multiplyNumeric(image));
