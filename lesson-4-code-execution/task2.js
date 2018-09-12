0502915490b0797a495f83f18825f2792c515252
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-12 20:48:47 +0300

    Modify task2 and task2 test

---
+++ lesson-4-code-execution/task2.js
@@ -0,0 +1,9 @@
+function multiplyNumeric(obj) {
+  for (var key in obj) {
+    if (typeof(obj[key]) === 'number') {
+      obj[key] *= 2;
+    }
+  }
+}
+var image = {width: 100,height: 400,title: 'Cool image'};
+console.log(multiplyNumeric(image));
---
+++ lesson-4-code-execution/task2.test.js
@@ -0,0 +1,6 @@
+describe('Object x2 function', () => {
+  it ('Test final object, if only numbers inside'), ()=> {
+    expect(multiplyNumeric(image = {width: 100,height: 400,title: 'Cool image'}))
+  .toBe(image = {width: 200,height: 800,title: 'Cool image'});
+  }
+})
