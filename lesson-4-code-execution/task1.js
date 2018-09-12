5bfb0a6c8c751cb8d37427a772c22b492100faec
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-12 14:54:05 +0300

    Complete task1

---
+++ lesson-4-code-execution/task1.js
@@ -0,0 +1,16 @@
+'use strict'
+var tasksCompleted = {
+  'Anna': 29,
+  'Serg': 35,
+  'Elena': 1,
+  'Anton': 99
+};
+var maxTasksCompleted = 0;
+var maxTasksCompletedName = '';
+for (var key in tasksCompleted) {
+  if (tasksCompleted[key] > maxTasksCompleted){
+    maxTasksCompleted = tasksCompleted[key];
+    maxTasksCompletedName = key;
+  }
+}
+console.log(maxTasksCompletedName + ' has maximum result with ' + maxTasksCompleted + ' tasks.');
