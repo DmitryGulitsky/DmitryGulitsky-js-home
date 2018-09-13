e6f352e7c0de55178a03aa5ac63305971111ac1a
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-13 14:29:10 +0300

    Modify tests

--- lesson-4-code-execution/task1.test.js
+++ lesson-4-code-execution/task1.test.js
@@ -1,9 +1,10 @@
-describe ('Find maximum completed tasks', () => {
-  it ('Find maximum completed tasks name'), () => {
+let maxTasksCompletedName = require('./task1');
+let maxTasksCompleted = require('./task1');
+describe('Find maximum completed tasks', () => {
+  it('Find maximum completed tasks name'), () => {
     expect(maxTasksCompletedName).toBe('Anton');
   }
-  it ('Find maximum completed tasks number'), () => {
+  it('Find maximum completed tasks number'), () => {
     expect(maxTasksCompleted).toBe(99);
   }
-  }
-)
+})
--- lesson-4-code-execution/task2.test.js
+++ lesson-4-code-execution/task2.test.js
@@ -1,6 +1,7 @@
+let multiplyNumeric = require('./task2');
+let image = require('./task2');
 describe('Object x2 function', () => {
-  it ('Test final object, if only numbers inside'), ()=> {
-    expect(multiplyNumeric(image = {width: 100, height: 400, title: 'Cool image'}))
-  .toBe(image = {width: 200, height: 800, title: 'Cool image'});
+  it('Test final object, if only numbers inside'), ()=> {
+    expect(multiplyNumeric(image)).toBe({width: 200, height: 800, title: 'Cool image'});
   }
 })
