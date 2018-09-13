3d8b26248974b11f457e1f1bc771ec648eaf9291
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-13 14:09:31 +0300

    Modify task3

--- lesson-4-code-execution/task3.js
+++ lesson-4-code-execution/task3.js
@@ -2,7 +2,6 @@
 var inputArray = [];
 do {
   var inputNumber = prompt('Введите число:');
-  console.log('Вводим input number:' + inputNumber);
   var parsedInputNumber = parseInt(inputNumber);
   if (isNaN(parsedInputNumber)) break;
   inputArray.push(inputNumber);
@@ -11,5 +10,4 @@ var sum = 0;
 for (var i = 0; i < inputArray.length; i++) {
   sum += +inputArray[i];
 }
-console.log(inputArray);
-console.log(sum);
+alert('Сумма введеных чисел: ' + sum)
