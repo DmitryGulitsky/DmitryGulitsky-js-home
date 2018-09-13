413c08e5d33805e5f763268d66286c16c39fe458
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-13 14:07:01 +0300

    complete task3

--- lesson-4-code-execution/task3.js
+++ lesson-4-code-execution/task3.js
@@ -1,8 +1,15 @@
 'use strict';
 var inputArray = [];
-var i = 0;
-while (inputArray[i] !== false || inputArray[i] === 'number' || inputArray[i] !== '') {
-  prompt(inputArray[i]);
-  i++;
+do {
+  var inputNumber = prompt('Введите число:');
+  console.log('Вводим input number:' + inputNumber);
+  var parsedInputNumber = parseInt(inputNumber);
+  if (isNaN(parsedInputNumber)) break;
+  inputArray.push(inputNumber);
+} while (true);
+var sum = 0;
+for (var i = 0; i < inputArray.length; i++) {
+  sum += +inputArray[i];
 }
 console.log(inputArray);
+console.log(sum);
