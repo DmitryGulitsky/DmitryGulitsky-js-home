35640ff91ebc97126f851423eda98689c65071af
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-15 20:06:16 +0300

    Delete index.html, add test

--- lesson-4-code-execution/task1.js
+++ lesson-4-code-execution/task1.js
@@ -1,13 +1,25 @@
 'use strict';
-var maxTaskExportFunction = function findMaxTasksCompleted(tasksCompleted) {
+function findMaxTasksCompletedName(tasksCompleted) {
   var maxTasksCompleted = 0;
   var maxTasksCompletedName = '';
+  var nameCounter = 0;
   for (var key in tasksCompleted) {
-    if (tasksCompleted[key] > maxTasksCompleted) {
+    if (tasksCompleted[key] === null) {
+      maxTasksCompletedName = 'Никто';
+    }
+    else if (tasksCompleted[key] > maxTasksCompleted) {
       maxTasksCompleted = tasksCompleted[key];
       maxTasksCompletedName = key;
     }
   }
+  for (key in tasksCompleted) {
+    if (tasksCompleted[key] === maxTasksCompleted) {
+      nameCounter++;
+    }
+  }
+  if (nameCounter > 1) {
+    maxTasksCompletedName = 'Ничья';
+  }
   return maxTasksCompletedName;
 }
-module.exports = maxTaskExportFunction;
+module.exports = findMaxTasksCompletedName;
--- lesson-4-code-execution/task1.test.js
+++ lesson-4-code-execution/task1.test.js
@@ -1,6 +1,13 @@
-let maxTaskExportFunction = require('./task1');
-describe('Find maximum completed tasks', () => {
-  it('Find maximum completed tasks', () => {
-    expect(maxTaskExportFunction({'Anna': 29, 'Serg': 35, 'Elena': 1, 'Anton': 99})).toBe('Anton');
+findMaxTasksCompletedName = require('./task1');
+describe('Function findMaxTasksCompletedName', () => {
+  it('Find one maximum completed tasks Name', () => {
+    expect(findMaxTasksCompletedName({'Anna': 29, 'Serg': 35, 'Elena': 1, 'Anton': 99})).toBe('Anton');
+  });
+  it('Find draw with equal completed tasks', () => {
+    expect(findMaxTasksCompletedName({'Anna': 99, 'Serg': 99, 'Elena': 1, 'Anton': 99})).toBe('Ничья');
+  });
+  it('Find nobody with completed tasks', () => {
+    var object = null;
+    expect(findMaxTasksCompletedName(object)).toBe('Никто');
   });
 });
