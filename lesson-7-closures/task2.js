cc886128e53a765853a04dcf8b9a37001a47585f
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-23 10:49:25 +0300

    Complete task2

---
+++ lesson-7-closures/task2.js
@@ -0,0 +1,17 @@
+function interviewQuestion(profession) {
+  function interviewQuestionWithProfession(name) {
+    if (profession === 'designer') {
+      var question = name + ' can you please explain what UX design is?';
+    } else if (profession === 'teacher') {
+      question = 'What subject do you teach ' + name + '?';
+    } else if  (profession === 'other') {
+      question = 'Hello ' + name + ', what do you do?';
+    }
+    return question;
+  }
+  return interviewQuestionWithProfession;
+}
+console.log(interviewQuestion('teacher')('John'));
+console.log(interviewQuestion('designer')('John'));
+console.log(interviewQuestion('other')('John'));
+
