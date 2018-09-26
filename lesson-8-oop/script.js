01150e2e5eae57b85732cce81847427d1ba3833c
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-09-26 22:36:32 +0300

    Correct question3

--- lesson-8-oop/script.js
+++ lesson-8-oop/script.js
@@ -4,7 +4,7 @@ function Question(HQuestion, _text, _answerVariants, _answerTrueVariant) {
   HQuestion.answerVariants = _answerVariants;
   HQuestion.answerTrueVariant = _answerTrueVariant;
   HQuestion.show = function () {
-    var userAnswer = +prompt(this.text);
+    var userAnswer = +prompt(this.text + this.answerVariants + '?');
     if (userAnswer === _answerTrueVariant) {
       console.log('You are absolutely right!');
     } else {
@@ -14,13 +14,13 @@ function Question(HQuestion, _text, _answerVariants, _answerTrueVariant) {
 }
 
 var Question1 = {};
-Question(Question1, 'How many corners at the square?', [0, 1, 2, 3, 4], 4);
+Question(Question1, 'How many corners at the square? ', [0, 3, 4, 5], 4);
 
 var Question2 = {};
-Question(Question2, 'How many angles does a triangle have?', [0, 1, 2, 3, 4], 3);
+Question(Question2, 'How many angles does a triangle have? ', [0, 3, 4, 5], 3);
 
 var Question3 = {};
-Question(Question3, 'How many corners are there in the circle?', [0, 1, 2, 3, 4], 0);
+Question(Question3, 'How many angles does a pentagon have?? ', [0, 3, 4, 5], 5);
 
 Question1.show();
 Question2.show();
