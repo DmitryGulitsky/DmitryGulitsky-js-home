e3ff14c22d89b76a59405797ec62823b0c49e077
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-02 22:59:07 +0300

    Add hashstorage.js

---
+++ lesson-10-dom/HashStorage.js
@@ -0,0 +1,23 @@
+function THashStorage(name) {
+  this.addValue = function(key, value) {
+    
+  };
+  this.getValue = function(key) {
+
+  };
+  this.deleteValue = function(key) {
+    
+  };
+  this.getKeys = function() {
+
+  };
+  this.name =
+}
+
+function User(name) {
+  this.name = name;
+}
+
+User.prototype.THashStorage = function() {
+  alert(this.name);
+};
