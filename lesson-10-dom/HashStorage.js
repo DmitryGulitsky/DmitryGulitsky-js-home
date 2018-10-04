1059c996a3bf43a6b8c335a7b255745548d92527
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-04 15:16:52 +0300

    Add script to index.html

--- lesson-10-dom/HashStorage.js
+++ lesson-10-dom/HashStorage.js
@@ -8,11 +8,7 @@ THashStorage.prototype.addValue = function (key, value) {
   THashStorage.drinkStorage.key = value;
 };
 
-function clickAddValue() {
-  var key = prompt('Input name of the drink, please');
-  var value = prompt('Input recipe of the drink, please');
-  var addNewDrink = new THashStorage(key, value);
-}
+
 
 clickAddValue();
 console.log(THashStorage);
