f5ae5809912084ad449cc709f2ba1f74700996ae
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-05 02:04:37 +0300

    Add recipe, boolean alco

--- lesson-10-dom/HashStorage.js
+++ lesson-10-dom/HashStorage.js
@@ -1,14 +1,12 @@
 var THashStorage = function () {
   this.addValue = function (key, value) {
     storage[key] = value;
-    console.log(storage);
   };
   this.getValue = function (key) {
     if (storage[key]) {
       console.log(storage[key]);
     } else {
       console.log('There is no such drink in the storage');
-      console.log(storage);
     }
   };
   this.deleteValue = function (key) {
@@ -16,7 +14,6 @@ var THashStorage = function () {
       delete storage[key];
     } else {
       console.log('There is no such drink in the storage');
-      console.log(storage);
     }
   };
   this.getKeys = function () {
