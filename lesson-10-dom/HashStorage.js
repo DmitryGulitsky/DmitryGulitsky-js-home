0172d87cf4b09bd8ecd75fe8adccf63cff483842
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-05 00:51:27 +0300

    Delete prototype

--- lesson-10-dom/HashStorage.js
+++ lesson-10-dom/HashStorage.js
@@ -1,14 +1,25 @@
-var THashStorage = function (key, value) {
-  this.key = key;
-  this.value = value;
-  var drinkStorage = {};
-};
-
-THashStorage.prototype.addValue = function (key, value) {
-  THashStorage.drinkStorage.key = value;
-};
-
-
-
-clickAddValue();
-console.log(THashStorage);
+var THashStorage = function () {
+  this.addValue = function (key, value) {
+    storage[key] = value;
+    console.log(storage);
+  };
+  this.getValue = function (key) {
+    if (storage[key]) {
+      console.log(storage[key]);
+    } else {
+      console.log('There is no such drink in the storage');
+      console.log(storage);
+    }
+  };
+  this.deleteValue = function (key) {
+    if (storage[key]) {
+      delete storage[key];
+    } else {
+      console.log('There is no such drink in the storage');
+      console.log(storage);
+    }
+  };
+  this.getKeys = function () {
+    console.log(Object.keys(storage));
+  };
+}
