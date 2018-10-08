253d32b421e8e692e49deb75011ce3c78a01b377
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-08 03:11:36 +0300

    Add form fields

--- lesson-11-events/script.js
+++ lesson-11-events/script.js
@@ -1,10 +1,19 @@
-
 (function () {
   function generateForm(parent, origForm, formFields) {
     var doc = document;
     var container = doc.body;
     var origForm = doc.forms.addSiteForm;
-    var formFields = [];
+    var formFields = [
+      {'type': 'text', 'label': 'Разработчики:', 'width': 500},
+      {'type': 'text', 'label': 'Название сайта:', 'width': 500},
+      {'type': 'url', 'label': 'URL сайта:', 'width': 500},
+      {'type': 'date', 'label': 'Дата запуска сайта:', 'width': 200},
+      {'type': 'number', 'label': 'Посетителей в сутки:', 'width': 200},
+      {'type': 'email', 'label': 'E-mail для связи:', 'width': 300},
+      {'type': 'select', 'label': 'Рубрика каталога:', 'option': {'бытовая техника': true, 'разное': true}, 'width': 300},
+      {'type': 'radio', 'label': 'Размещение:', 'radioLabel': {'бесплатное': true, 'платное': true, 'VIP': true}},
+      {'type': 'checkbox', 'label': 'Разрешить отзывы:'},
+      {'type': 'textarea', 'label': 'Описание сайта:', 'width': 500, 'height': 200}, {'type': 'submit', 'label': 'Опубликовать'}];
     generateForm(parent, origForm, formFields);
   }
 })();
