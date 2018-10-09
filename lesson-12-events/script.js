f785837161e3cccbfaae0bce251508f3a42c18b8
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-10 01:12:02 +0300

    Add position absolute function

--- lesson-12-events/script.js
+++ lesson-12-events/script.js
@@ -5,14 +5,19 @@
     var imageElems = document.getElementsByTagName('img');
     var positionArray = [];
     for (var i = 0; i < imageElems.length; i++) {
-      positionArray = imageElems[i].getBoundingClientRect();
+      positionArray.push(imageElems[i].getBoundingClientRect());
+      var imageElem = imageElems[i];
+      var xPosition = positionArray[i].x;
+      var yPosition = positionArray[i].y;
+      imageElem.style.position = 'absolute';
+      imageElem.style.left = xPosition.toString();
+      imageElem.style.top = yPosition.toString();
+      console.log(positionArray[i]);
+      console.log(positionArray[i].x);
+      console.log(positionArray[i].y);
       console.log(positionArray);
     }
     console.log(imageElems);
-    for (i = 0; i < imageElems.length; i++) {
-      var imageElem = imageElems[i];
-      imageElem.style.position = 'absolute';
-    }
   }
   imgToPositionAbsolute();
 })();
