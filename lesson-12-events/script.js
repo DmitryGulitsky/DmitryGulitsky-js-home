a1e31237afb397ddf89d383652bb13393b0f2d03
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-12 00:22:40 +0300

    Modify code

--- lesson-12-events/script.js
+++ lesson-12-events/script.js
@@ -5,6 +5,7 @@
     for (var i = 0; i < imageElems.length; i++) {
       imageElems[i].style.left = imageElems[i].offsetLeft + 'px';
       imageElems[i].style.top = imageElems[i].offsetTop + 'px';
+      imageElems[i].style.zIndex = '0';
     }
   }
 
@@ -16,13 +17,13 @@
 
   function setDragAttributes() {
     for (var i = 0; i < imageElems.length; i++) {
-      imageElems[i].addEventListener('mousedown', dragStart, false);
-      imageElems[i].addEventListener('mouseon', dragEnd, false);
+      imageElems[i].addEventListener('mousedown', dragStart, true);
+      imageElems[i].addEventListener('mouseup', dragEnd, true);
     }
-    console.log(imageElems);
   }
 
   var imageElems = document.getElementsByTagName('img');
+  var imageContainer = document.getElementsByTagName('div');
   var DragImage = null; // какая картинка сейчас перетаскивается
   var DragImgX = null; // координата левого верхнего угла картинки по Х
   var DragImgY = null; // координата левого верхнего угла картинки по Y
@@ -37,6 +38,9 @@
   function dragStart(EO) {
     EO = EO || window.event;
     DragImage = EO.target;
+    DragImage.style.cursor = 'move';
+    imgZindex++;
+    DragImage.style.zIndex = imgZindex.toString();
     DragImgX = DragImage.offsetLeft;
     DragImgY = DragImage.offsetTop;
     DragClickX = EO.pageX;
@@ -44,6 +48,10 @@
     DragShiftX = DragClickX - DragImgX;
     DragShiftY = DragClickY - DragImgY;
     DragImage.addEventListener('mousemove', dragMove, true);
+    DragImage.addEventListener('mouseleave', dragMove, true);
+    DragImage.addEventListener('mouseover', dragMove, true);
+    DragImage.addEventListener('mouseout', dragMove, true);
+    DragImage.addEventListener('mouseenter', dragMove, true);
     console.log('DragImgX = ' + DragImgX);
     console.log('DragImgY = ' + DragImgY);
     console.log('DragClickX = ' + DragClickX);
@@ -65,11 +73,13 @@
   function dragEnd(EO) {
     EO = EO || window.event;
     DragImage = EO.target;
-    imgZindex++;
-    DragImage.style.zIndex = imgZindex.toString();
-    console.log('DropClickX = ' + DropClickX);
-    console.log('DropClickY = ' + DropClickY);
-    console.log('zIndex = ' + imgZindex);
+    EO.preventDefault();
+    DragImage.removeEventListener('mousemove', dragMove, true);
+    DragImage.removeEventListener('mouseleave', dragMove, true);
+    DragImage.removeEventListener('mouseover', dragMove, true);
+    DragImage.removeEventListener('mouseout', dragMove, true);
+    DragImage.removeEventListener('mouseenter', dragMove, true);
+    DragImage.style.cursor = 'default';
     console.log('drag finished');
     console.log('----------------------------');
     DragImage = null;
@@ -82,6 +92,7 @@
     DragShiftX = null;
     DragShiftY = null;
   }
+
   getImgPosition();
   imgToPositionAbsolute();
   setDragAttributes();
