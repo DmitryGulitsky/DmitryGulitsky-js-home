278ee58382df913f045bf3c835a8b6135778d4b9
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-10-14 21:24:11 +0300

    Correct task

--- lesson-9-oop/task1.js
+++ lesson-9-oop/task1.js
@@ -1,48 +1,68 @@
+'use strict';
 (function () {
-  function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
+  function Question(text, answerVariants, answerTrueVariant) {
+    this.text = text;
+    this.answerVariants = answerVariants;
+    this.answerTrueVariant = answerTrueVariant;
   }
 
   Question.prototype.displayQuestion = function () {
-    console.log(this.question);
-    for (var i = 0; i < this.answers.length; i++) {
-      console.log(i + ': ' + this.answers[i]);
+    console.log(this.text);
+
+    for (var i = 0; i < this.answerVariants.length; i++) {
+      console.log(i + ': ' + this.answerVariants[i]);
     }
   };
 
-  Question.prototype.checkAnswer = function (ans) {
-    if (ans === this.correct) {
+  Question.prototype.checkAnswer = function (ans, callback) {
+    var sc;
+    if (ans === this.answerTrueVariant) {
       console.log('Correct answer!');
-      pointsCounter++;
-      console.log('Your score: ' + pointsCounter + ' points!');
+      sc = callback(true);
     } else {
       console.log('Wrong answer. Try again :)');
-      console.log('Your score: ' + pointsCounter + ' points!');
+      sc = callback(true);
     }
+    this.displayScore(sc);
   };
 
-  var q1 = new Question('Is JavaScript the coolest programming language in the world?',
-    ['Yes', 'No'],
-    0);
-  var q2 = new Question('What is the name of this course\'s teacher?',
-    ['John', 'Micheal', 'Jonas'],
-    2);
-  var q3 = new Question('What does best describe coding?',
-    ['Boring', 'Hard', 'Fun', 'Tediuos'],
-    2);
+  Question.prototype.displayScore = function (score) {
+    console.log('Your current score is: ' + score);
+    console.log('------------------------------');
+  };
 
-  var questions = [q1, q2, q3];
-  var pointsCounter = 0;
-  function askQuestion() {
-    var n = Math.floor(Math.random() * questions.length);
-    questions[n].displayQuestion();
-    var answer = +prompt('Please select the correct answer:');
+  function score() {
+    var sc = 0;
+    return function (answerTrueVariant) {
+      if (answerTrueVariant) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+
+  var keepScore = score();
+
+  var question1 = new Question('How many corners at the square? ', [0, 3, 4, 5], 3);
+  var question2 = new Question('How many angles does a triangle have? ', [0, 3, 4, 5], 2);
+  var question3 = new Question('How many angles does a pentagon have? ', [0, 3, 4, 5], 4);
+  var questionArray = [question1, question2, question3];
+
+  function nextQuestion() {
+    var n = Math.floor(Math.random() * questionArray.length);
+
+    questionArray[n].displayQuestion();
+
+    var answer = parseInt(prompt('Please select the correct answer.'));
+
+    questionArray[n].checkAnswer(answer);
+
     if (answer !== 'exit') {
-      questions[n].checkAnswer(parseInt(answer));
-      askQuestion();
+      questionArray[n].checkAnswer(parseInt(answer), keepScore);
+
+      nextQuestion();
     }
   }
-  askQuestion();
+
+  nextQuestion();
 })();
