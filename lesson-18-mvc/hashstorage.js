84e0f81f51655a18e7ce14452439d16584ba77b7
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-11-02 00:40:50 +0300

    Add local storage

--- lesson-18-mvc/hashstorage.js
+++ lesson-18-mvc/hashstorage.js
@@ -1,20 +1,48 @@
 function THashStorage() {
-  var self = this,
-    pHash = {};
+  var self = this;
 
   self.addValue = function(key, value) {
-    pHash[key] = value;
+    self.pHash[key] = value;
   };
 
   self.getValue = function(key) {
-    return pHash[key];
+    return self.pHash[key];
   };
 
   self.deleteValue = function(key) {
-    return delete pHash[key];
+    return delete self.pHash[key];
   };
 
   self.getKeys = function() {
-    return (Object.keys(pHash));
+    return (Object.keys(self.pHash));
+  };
+}
+
+function TLocalStorage(localStorageKey) {
+  var self = this,
+    pHash = {};
+
+  self.reset = function (storageName) {
+    if (window.localStorage.getItem(localStorageKey)) {
+      storageName.pHash = JSON.parse(JSON.stringify(JSON.parse(window.localStorage.getItem(localStorageKey))));
+    }
+  };
+
+  self.addValue = function (key, value) {
+    if (window.localStorage.getItem(localStorageKey)) {
+      pHash = JSON.parse(window.localStorage.getItem(localStorageKey));
+    }
+    pHash[key] = value;
+    window.localStorage.setItem(localStorageKey, JSON.stringify(pHash));
+  };
+
+  self.deleteValue = function (key) {
+    if (window.localStorage.getItem(localStorageKey)) {
+      pHash = JSON.parse(window.localStorage.getItem(localStorageKey));
+    }
+    if (pHash[key]) {
+      pHash.deleteValue(key);
+    }
+    window.localStorage.setItem(localStorageKey, JSON.stringify(pHash));
   };
 }
